53089ded654ab48ad486de1bd5eb6a21736a0d00
lynksalpha <161200418+lynksalpha@users.noreply.github.com>
2024-02-26 14:31:08 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $(".tenants").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    infinite: true,
+    fade: true,
+    arrows: false,
+    pauseOnFocus: false,
+    cssEase: "linear",
+  });
+});
+
+
